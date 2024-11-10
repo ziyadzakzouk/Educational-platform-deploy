8dec8cb48983aa93d8a86d10e367184fdc3a9dfb
Youssef Ashraf <155368101+Youssef-Ashraf2099@users.noreply.github.com>
2024-11-10 22:08:12 +0200

    Add files via upload

---
+++ SRC Quireies/TableScript.sql
@@ -0,0 +1,21 @@
+﻿
+
+CREATE TABLE Learner (
+    Learner_ID INT PRIMARY KEY,
+    name VARCHAR(50),
+    birthday DATE,
+   
+    gender VARCHAR(10),
+    country VARCHAR(50)
+    
+);
+
+CREATE TABLE Course (
+    Course_ID INT PRIMARY KEY,
+    title VARCHAR(100),
+    description VARCHAR(255),
+    diff_level VARCHAR(8),
+    credit_point INT,
+    instructor_ID INT
+    --FOREIGN KEY (instructor_ID) REFERENCES Instructor(ID)
+);
