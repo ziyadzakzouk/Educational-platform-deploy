93f21836eb8588dc90a774fd827315cc8884b764
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-11-17 21:30:56 +0200

    personal profile

--- SRC Quireies/InsertScript.sql
+++ SRC Quireies/InsertScript.sql
@@ -1,3 +1,7 @@
 ﻿-- Insert data into Learner table
 INSERT INTO Learner (Learner_ID, Name, Email, DateOfBirth)
 VALUES (1, 'John Doe', 'john.doe@example.com', '1990-01-01');
+
+-- Insert data into PersonalProfile table
+INSERT INTO PersonalProfile (Profile_ID, Learner_ID, ProfileName, ProfileDescription)
+VALUES (1, 1, 'Main Profile', 'This is the main profile of John Doe.');
