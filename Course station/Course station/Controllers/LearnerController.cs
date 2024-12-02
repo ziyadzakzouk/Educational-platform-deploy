e9d048501ede328dd76f8c129b8a703347b7b441
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-03 00:03:34 +0200

    learner controller CRUD added

---
+++ Course station/Course station/Controllers/LearnerController.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Course_station.Models;
+
+namespace Course_station.Controllers
+{
+    public class LearnerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LearnerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Learner
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Learners.ToListAsync());
+        }
+
+        // GET: Learner/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var learner = await _context.Learners
+                .FirstOrDefaultAsync(m => m.LearnerId == id);
+            if (learner == null)
+            {
+                return NotFound();
+            }
+
+            return View(learner);
+        }
+
+        // GET: Learner/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Learner/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LearnerId,FirstName,LastName,Birthday,Gender,Country,CulturalBackground")] Learner learner)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(learner);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(learner);
+        }
+
+        // GET: Learner/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var learner = await _context.Learners.FindAsync(id);
+            if (learner == null)
+            {
+                return NotFound();
+            }
+            return View(learner);
+        }
+
+        // POST: Learner/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LearnerId,FirstName,LastName,Birthday,Gender,Country,CulturalBackground")] Learner learner)
+        {
+            if (id != learner.LearnerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(learner);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LearnerExists(learner.LearnerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(learner);
+        }
+
+        // GET: Learner/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var learner = await _context.Learners
+                .FirstOrDefaultAsync(m => m.LearnerId == id);
+            if (learner == null)
+            {
+                return NotFound();
+            }
+
+            return View(learner);
+        }
+
+        // POST: Learner/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var learner = await _context.Learners.FindAsync(id);
+            _context.Learners.Remove(learner);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LearnerExists(int id)
+        {
+            return _context.Learners.Any(e => e.LearnerId == id);
+        }
+    }
+}
+
+
+
+
--- Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64802d8a244c56d2be97730fbc453ce3757f6fe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd51a78effff79afcb7eb56362fb2dd1bda8060")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
