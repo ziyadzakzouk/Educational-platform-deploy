90e0cd78b547252e17adf8f85cb6b19e6b206f80
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-03 07:56:41 +0200

    learnerDisscussion controller

--- Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08f8dd6e9af8bba8f4e6ce6cbdd4194ce6fa98c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3a97004d4b70668650c8818035b2d0ce6fa36e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
