9b667e49cbd8ef027191a21c5cbb72f7174db62c
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-03 07:48:44 +0200

    Instructor count controller

--- Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02242301af887208b61c4a79f39723103e1ec38d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+835332d7e88bd05fbc5c5db8b16d8f9ba7d71a63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
