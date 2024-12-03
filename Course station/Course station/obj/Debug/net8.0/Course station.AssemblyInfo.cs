03ae440f3b0fedfee5d1ef73cb910540b5cb0f7d
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-03 06:56:00 +0200

    Healthcontroller class is added

--- Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d048501ede328dd76f8c129b8a703347b7b441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dc074d2a09ef03ad7ce85c1ac45337d66aa4a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
