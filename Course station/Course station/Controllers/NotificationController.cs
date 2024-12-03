2235053672efb23dab7531c33ba29a0609a4d97b
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-03 12:25:26 +0200

    notfication controller added

---
+++ Course station/Course station/Controllers/NotificationController.cs
@@ -0,0 +1,145 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Course_station.Models;
+
+namespace Course_station.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotificationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notification
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Notifications.ToListAsync());
+        }
+
+        // GET: Notification/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications
+                .FirstOrDefaultAsync(m => m.NotificationId == id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return View(notification);
+        }
+
+        // GET: Notification/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Notification/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NotificationId,TimeStamp,Message,Urgency,Readstatus")] Notification notification)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(notification);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(notification);
+        }
+
+        // GET: Notification/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications.FindAsync(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+            return View(notification);
+        }
+
+        // POST: Notification/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("NotificationId,TimeStamp,Message,Urgency,Readstatus")] Notification notification)
+        {
+            if (id != notification.NotificationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(notification);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NotificationExists(notification.NotificationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(notification);
+        }
+
+        // GET: Notification/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications
+                .FirstOrDefaultAsync(m => m.NotificationId == id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return View(notification);
+        }
+
+        // POST: Notification/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+            _context.Notifications.Remove(notification);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NotificationExists(int id)
+        {
+            return _context.Notifications.Any(e => e.NotificationId == id);
+        }
+    }
+}
