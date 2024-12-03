565e4807b2100e95ba59277b0cd0fbeacdb914fe
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-03 08:10:16 +0200

    Module controller added

--- Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e99e68116de7de99c3a11d2a5e69932abd76da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aff423ffb73c1df7d83ab53d1fa05e591be5112")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
