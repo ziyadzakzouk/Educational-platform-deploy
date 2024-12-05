464089f6edd82bf37cf6339ee13a6b335c51af5e
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-06 00:54:21 +0200

    instrucor view changed

--- Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e531d8a6bce4c6f9d34a03c4ce42a81855e22e8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5933e391be530ee61656bec0ebd2f2f200e519fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
