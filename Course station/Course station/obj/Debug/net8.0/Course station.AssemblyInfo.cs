f9d712f993149f3f840468f13cbbb6dc3897f1a2
Abdelrahman Mostafa <151868068+moussa101@users.noreply.github.com>
2024-12-06 09:07:37 -0800

    created delete confirmed

--- Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4532e6c5fb6e76e6407bc3544eefc28658987b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f56162b54d6d3b26410a7ad62c4c6e4271ba30f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
