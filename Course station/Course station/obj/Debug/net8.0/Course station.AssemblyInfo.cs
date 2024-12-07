2ecc561a8d5b203f8001a34c5bfa62ed1f5c8525
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-07 10:40:39 +0200

    index fix

--- Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net8.0/Course station.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de0e3657badaecff1a617a8a32be1c181ac42b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775b82d338ca803744ea50846a3d1906b8b1db1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
