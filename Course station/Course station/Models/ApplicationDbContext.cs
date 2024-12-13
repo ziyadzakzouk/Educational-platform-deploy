db664a02f4bd8375c4f5b0f9490471a732d4ad46
Moussa <abdelrhanmoussa21@gmail.com>
2024-12-13 20:45:11 +0200

    Db redirect update

--- Course station/Course station/Models/ApplicationDbContext.cs
+++ Course station/Course station/Models/ApplicationDbContext.cs
@@ -98,7 +98,7 @@ public partial class ApplicationDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=YOUSSEF2099XX\\JOE_PC;Database=CourseStation;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=EducationalPlatform;User Id=sa;Password=Password_123;Encrypt=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
