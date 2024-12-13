3c5a0b26c6fe3c4e7b457c3c2582cc4bc3f3488e
Youssef-Ashraf2099 <ya1684657@gmail.com>
2024-12-13 07:04:30 +0200

    notfication status added to learner with the current course he is enrolled in and log in is must and cannot access the home page directely

---
+++ Course station/Course station/Models/LearnerInfoViewModel.cs
@@ -0,0 +1,8 @@
+namespace Course_station.Models
+{
+    public class LearnerInfoViewModel
+    {
+        public Learner Learner { get; set; }
+        public List<Notification> Notifications { get; set; }
+    }
+}
