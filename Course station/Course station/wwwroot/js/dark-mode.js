81066390f618f83bee3861131dbd8403d488e145
Moussa <abdelrhanmoussa21@gmail.com>
2024-12-21 22:53:09 +0200

    retrived the old nav bar

--- Course station/Course station/wwwroot/js/dark-mode.js
+++ Course station/Course station/wwwroot/js/dark-mode.js
@@ -5,26 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
     // Check if dark mode is enabled in local storage
     if (localStorage.getItem('darkMode') === 'enabled') {
         enableDarkMode();
-        darkModeToggle.checked = true;
+    } else {
+        disableDarkMode();
     }
 
-    darkModeToggle.addEventListener('change', function () {
-        if (darkModeToggle.checked) {
-            enableDarkMode();
-        } else {
+    darkModeToggle.addEventListener('click', function () {
+        if (document.body.classList.contains('dark-theme')) {
             disableDarkMode();
+        } else {
+            enableDarkMode();
         }
     });
 
     function enableDarkMode() {
         document.body.classList.add('dark-theme');
         darkModeCss.disabled = false;
+        darkModeToggle.textContent = 'Toggle Light Mode';
         localStorage.setItem('darkMode', 'enabled');
     }
 
     function disableDarkMode() {
         document.body.classList.remove('dark-theme');
         darkModeCss.disabled = true;
+        darkModeToggle.textContent = 'Toggle Dark Mode';
         localStorage.setItem('darkMode', 'disabled');
     }
 });
